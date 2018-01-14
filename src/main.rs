b764e580244a7562a95afb260ac589708ac1bf67
doghere <doghere@tutamail.com>
2018-01-14 22:18:24 +0800

    add main

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate bst_rs;
+
+fn main() {
+    let mut tree = Tree::new();
+    tree.push("a")
+        .push("b")
+        .push("5")
+        .push("0")
+        .push("6")
+        .push("6")
+        .push("z");
+
+    println!("{:?}", tree);
+    println!("{:?}", tree.min());
+    println!("{:?}", tree.max());
+    println!("{:?}", tree.exists(&"12"));
+    println!("{:?}", tree.exists(&"z"));
+    println!("{:?}", tree.find(&"123"));
+    println!("{:?}", tree.find(&"a"));
+
+    tree.delete(&"0")
+        .delete(&"10")
+        .delete(&"b")
+        .delete(&"a");
+
+    println!("{:?}", tree);
+}
